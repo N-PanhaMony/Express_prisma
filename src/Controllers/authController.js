71f2ec04c3597262532dbc20b19c1103d3e897c1
N-PanhaMony <neangpanhamony001@gmail.com>
2026-01-24 16:49:03 +0700

    feat(auth): register endpoint working with request body

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,8 +1,45 @@
+import {prisma} from "../config/prisma.js";
+import bcrypt from "bcryptjs";
 
 const register = async (req, res) => {
-  const body = req.body;
-  res.json(body);
-    
-}
+  const {name , email , password} = req.body;
+  
+  // Check if user already exists
+  const userExits = await prisma.user.findUnique({
+    where: {
+      email: email
+    }
+  });
+
+  if(userExits){
+    return res.status(400).json({error: "User already exists"});
+  }
+  
+
+  // hash the password before saving (omitted for brevity)
+  // salt is used to enhance security of the hashed password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password , salt)
+
+  // Create new user
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+  
+  res.status(201).json({
+    message: "User registered successfully",
+    status: "success",
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    }
+  })
+
+} 
 
 export { register };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register } from '../Controllers/authController.js';
+import { register } from '../controllers/authController.js';
 
 const router = express.Router();
 
