b69a91284151b7a52360013cc579c6e0d3f7b49d
N-PanhaMony <neangpanhamony001@gmail.com>
2026-01-24 16:23:14 +0700

    feat: test auth register controller and route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import postsRouter from './routes/posts.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Mount the posts routes
 app.use('/posts', postsRouter);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
